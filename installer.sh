19e234f6d797c5839ef69e4f03a110e0d5f9d80f
upnt-unix <base.shun0329@gmail.com>
2021-03-04 14:21:02 +0900

    error fix

--- installer.sh
+++ installer.sh
@@ -1,3 +1,7 @@
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > nvim
-chmod a+x nvim
-mv nvim /usr/local/bin
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz > nvim-linux64.tar.gz
+tar -zxvf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+mv -n nvim-linux64/bin/* /usr/local/bin
+mv -n nvim-linux64/lib/* /usr/local/lib
+mv -n nvim-linux64/share/* /usr/local/share
+rm -rf nvim-linux64
