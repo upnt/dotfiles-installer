65d568d444a3a2b675f84f2a95b88d7a408e121c
upnt <base.shun0329@gmail.com>
2021-03-07 15:18:14 +0900

    error fix

--- installer.ps1
+++ installer.ps1
@@ -1,4 +1,6 @@
 Invoke-WebRequest https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip -OutFile nvim-win64.zip
 Expand-Archive -Path ./nvim-win64.zip -DestinationPath %UserProfile%
-$Env:Path += "%UserProfile%\Neovim\bin;"
+$environmentPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
+$environmentPath += "%UserProfile%\Neovim\bin;"
+[System.Environment]::SetEnvironmentVariable("Path", $environmentPath, "User")
 echo $Env:Path
