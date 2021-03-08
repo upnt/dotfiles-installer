bf17322ecadad4885f759e981d71cde63ad73b69
upnt-unix <base.shun0329@gmail.com>
2021-03-08 20:07:03 +0900

    enable to use dein.vim for linux

--- installer.sh
+++ installer.sh
@@ -1,7 +1,11 @@
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz > nvim-linux64.tar.gz
-tar -zxvf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-mv -n nvim-linux64/bin/* /usr/local/bin
-mv -n nvim-linux64/lib/* /usr/local/lib
-mv -n nvim-linux64/share/* /usr/local/share
-rm -rf nvim-linux64
+mkdir tmp
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz > tmp/nvim-linux64.tar.gz
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > tmp/installer.sh
+tar -zxvf tmp/nvim-linux64.tar.gz -C tmp
+
+mv -n tmp/nvim-linux64/bin/* /usr/local/bin
+mv -n tmp/nvim-linux64/lib/* /usr/local/lib
+mv -n tmp/nvim-linux64/share/* /usr/local/share
+sh tmp/installer.sh ~/.cache/dein
+
+rm -rf tmp
