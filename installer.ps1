7cf7f2b40f17a1c5b764b61de0d4cf901c94d07c
upnt <base.shun0329@gmail.com>
2021-03-08 19:27:57 +0900

    bug fix

--- installer.ps1
+++ installer.ps1
@@ -1,5 +1,7 @@
+$userprofile = $Env:UserProfile
 Invoke-WebRequest https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip -OutFile nvim-win64.zip
-Expand-Archive -Path ./nvim-win64.zip -DestinationPath %UserProfile%
+Expand-Archive -Path ./nvim-win64.zip -DestinationPath $Env:UserProfile/nvim-win64
 $environmentPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
-$environmentPath += "%UserProfile%\Neovim\bin;"
+$environmentPath = $Env:UserProfile + "\nvim-win64\Neovim\bin;" + $environmentPath
 [System.Environment]::SetEnvironmentVariable("Path", $environmentPath, "User")
+rm nvim-win64.zip
