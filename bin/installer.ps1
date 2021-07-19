40b0b2e8fd4ff70b0c80cb25bf427bd77fdbeb4a
upnt <upnt.github@gmail.com>
2021-07-20 07:50:01 +0900

    add providers(nodejs-lts, perl, ruby, anaconda3)

--- bin/installer.ps1
+++ bin/installer.ps1
@@ -14,6 +14,12 @@ if ( -not ( Test-Path -Path $userprofile/scoop ) ) {
 scoop install git
 scoop bucket add extras
 scoop install neovim
+scoop install neovide
+scoop install anaconda3
+scoop install nodejs-lts
+scoop install perl
+scoop install ruby
+scoop install yarn
 
 # install dein.vim
 if ( -not ( Test-Path -Path $userprofile/.cache/dein ) ) {
@@ -21,11 +27,11 @@ if ( -not ( Test-Path -Path $userprofile/.cache/dein ) ) {
     .\tmp\installer.ps1 ~/.cache/dein
 }
 
-# install some plugins(include builtin lsp)
+# install some plugins
 if ( -not ( Test-Path -Path $userprofile/AppData/Local/nvim ) ) {
     $ConfigDir = (New-Item $userprofile/AppData/Local/nvim -ItemType Directory).FullName
     git clone https://github.com/upnt/neovim-config $ConfigDir
-
+    scoop install code-minimap
 }
 
 # remove
