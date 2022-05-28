1d910f53600a0adf4fc3170bd74dd14fc676773f
upnt <34807332+upnt@users.noreply.github.com>
2022-05-28 16:22:04 +0900

    Delete windows setup script

--- bin/installer.ps1
+++
@@ -1,5 +0,0 @@
-# install some plugins
-if ( -not ( Test-Path -Path $Env:LocalAppData/nvim ) ) {
-    $ConfigDir = (New-Item $Env:LocalAppData/nvim -ItemType Directory).FullName
-    git clone https://github.com/upnt/neovim-config $ConfigDir
-}
